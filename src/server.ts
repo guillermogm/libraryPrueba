89f4405231506e022ce4cabce1be80798dad8bdb
guillermogm <guillermogm009@gmail.com>
2024-06-28 12:41:45 +0200

    feat: authors crud

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,32 @@ import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 4000
+// middleware
+app.use(express.json())
 
 app.get('/healthy', (req,res)=>{
     res.send('Server is healthy')
 })
+//Authors
+app.get('/authors', (req,res)=>{
+    res.send('Get all authors')
+})
 
+app.post('/authors',(req, res) =>{
+    console.log(req.body)
+    console.log(req.body.name)
+    res.send('Author created')
+})
+
+app.put('/authors/:id',(req, res) =>{
+    res.send(`Author updated con id: ${req.params.id}`)
+})
+
+app.delete('/authors/:id',(req, res) =>{
+    res.send(`Author deleted con id: ${req.params.id}`)
+})
+
+// Books
 app.get('/books', (req,res)=>{
     res.send('Get all books')
 })
