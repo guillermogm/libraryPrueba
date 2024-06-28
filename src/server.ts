6b9af36b061b4562d70986ecd42f644aebbb8e88
guillermogm <guillermogm009@gmail.com>
2024-06-28 11:57:27 +0200

    feat: crud books

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,18 @@ app.get('/books', (req,res)=>{
     res.send('Get all books')
 })
 
+app.post('/books',(req, res) =>{
+    res.send('Book created')
+})
+
+app.put('/books',(req, res) =>{
+    res.send('Book updated')
+})
+
+app.delete('/books',(req, res) =>{
+    res.send('Book deleted')
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
 })
