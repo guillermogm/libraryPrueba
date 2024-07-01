9c09862a79fe8fdd64dd5161f3418dc886af05f8
guillermogm <guillermogm009@gmail.com>
2024-07-01 11:04:47 +0200

    feat: Conexion to database

--- src/server.ts
+++ src/server.ts
@@ -2,51 +2,52 @@ import 'dotenv/config';
 import express from 'express';
 import { createdAuthor, deleteAuthorById, getAllAuthor, getAuthor, updateAuthorById } from './controllers/authors.controller';
 import { createBook, deleteBookById, getAllBooks, getBookById, updateBookById } from './controllers/books.controller';
-import { createLoans, getAllLoans,updateLoanById, getLoanById, deleteLoanById } from './controllers/loans.controller';
+import { createLoans, getAllLoans, updateLoanById, getLoanById, deleteLoanById } from './controllers/loans.controller';
 import { createUser, getUserProfile, updateUserById, deleteUserById, getUserById } from './controllers/user.controller';
 import { logInUser, signInUser } from './controllers/auth.controller';
+import { AppDataSource } from './database/db';
 
 const app = express();
 const PORT = process.env.PORT || 4000
 // middleware
 app.use(express.json())
 
-app.get('/healthy', (req,res)=>{
+app.get('/healthy', (req, res) => {
     //res.send('Server is healthy')
     res.status(205).json({
-        success:true,
-        message:"Server is healthy"
+        success: true,
+        message: "Server is healthy"
     })
 })
 //Authors
 app.get('/api/authors', getAllAuthor)
 app.post('/api/authors', createdAuthor)
-app.put('/api/authors/:id',updateAuthorById)
-app.delete('/authors/:id',deleteAuthorById)
+app.put('/api/authors/:id', updateAuthorById)
+app.delete('/authors/:id', deleteAuthorById)
 app.get('/api/author/:id', getAuthor)
 
 
 // Books
 app.get('/api/books', getAllBooks)
-app.post('/api/books',createBook)
-app.put('/api/books/:id',updateBookById)
-app.delete('/api/books/:id',deleteBookById)
+app.post('/api/books', createBook)
+app.put('/api/books/:id', updateBookById)
+app.delete('/api/books/:id', deleteBookById)
 app.get('/api/books/:id', getBookById)
 
 // Loans
 
 app.get('/api/books', getAllLoans)
-app.post('/api/books',createLoans)
-app.put('/api/books/:id',updateLoanById)
-app.delete('/api/books/:id',deleteLoanById)
+app.post('/api/books', createLoans)
+app.put('/api/books/:id', updateLoanById)
+app.delete('/api/books/:id', deleteLoanById)
 app.get('/api/books/:id', getLoanById)
 
 // Users
 
 app.get('/api/users/profile', getUserProfile)
-app.post('/api/users/',createUser)
-app.put('/api/users/:id',updateUserById)
-app.delete('/api/users/:id',deleteUserById)
+app.post('/api/users/', createUser)
+app.put('/api/users/:id', updateUserById)
+app.delete('/api/users/:id', deleteUserById)
 app.get('/api/users/:id', getUserById)
 
 // Auth
@@ -54,7 +55,15 @@ app.post('/api/auth/register', signInUser)
 app.post('/api/auth/login', logInUser)
 
 
-// Sever listening
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
+AppDataSource.initialize()
+    .then(() => {
+        // Sever listening
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+
+        console.log('Database connected');
+    })
+    .catch(error => {
+        console.log(error)
+    })
