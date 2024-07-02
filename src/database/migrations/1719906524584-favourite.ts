3cb3640788a80e285554ed9a420b134066ff888f
guillermogm <guillermogm009@gmail.com>
2024-07-02 10:09:44 +0200

    feat: favourites migration

---
+++ src/database/migrations/1719906524584-favourite.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table, TableUnique } from "typeorm";
+
+export class Favourite1719906524584 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "favourites",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "user_id",
+                        type: "int",
+                    },
+                    {
+                        name: "book_id",
+                        type: "int",
+                    },
+                ],
+                foreignKeys:[{
+                    columnNames:['user_id'],
+                    referencedTableName:'users',
+                    referencedColumnNames:['id']
+                },
+                {columnNames:['book_id'],
+                referencedTableName:'books',
+                referencedColumnNames:['id']
+                }],
+                uniques: [
+                    new TableUnique({
+                        name: "user_book_unique",
+                        columnNames: ["user_id", "book_id"],
+                    }),
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('favourites')
+    }
+
+}
